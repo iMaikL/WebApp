144079c6919a311e04ee3742d15b67f7feae598b
iMaikL <Klomp@mbpvanmeenards3.home>
2014-09-30 19:46:34 +0200

    Added img directive
    Added img directive

--- static/js/app.js
+++ static/js/app.js
@@ -10,16 +10,30 @@ var MOVIEAPP = MOVIEAPP || {}; //Namespace
 
 		about: {
 			title:'about',
-			description: 'description here'
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non purus sollicitudin, pretium velit sit amet, facilisis orci. Aliquam nec feugiat turpis. Cras a nibh sit amet orci mattis sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet massa at condimentum cursus. Aliquam eu sagittis eros. Suspendisse lacinia fringilla pellentesque. Nullam sodales nisi vitae egestas commodo. Nullam tincidunt neque in euismod malesuada. Aliquam dui neque, porttitor ac est at, vulputate pellentesque orci. Sed ultrices pharetra magna, sit amet viverra libero sagittis non. Integer nunc arcu, viverra nec metus laoreet, venenatis lacinia lectus. Vestibulum pulvinar ultricies sapien, sit amet commodo felis euismod nec. In malesuada neque vitae nibh luctus maximus. Nam volutpat, erat sed fringilla facilisis, turpis lectus finibus velit, nec tristique ipsum sem vel lacus.'
 		},
 
 		movies: {
 			title: 'movies',
 			items: [
-				{ title: "Guardians of the Galaxy", releaseDate: "15 September 2014", description: "description here", cover: "image here" }
+				{ title: "Guardians of the Galaxy", releaseDate: "15 September 2014", description: "description here", image: "https://pbs.twimg.com/profile_images/496363117681455106/X2aE1GcC_reasonably_small.jpeg" },
+				{ title: "Guardians of the Galaxy", releaseDate: "15 September 2014", description: "description here", image: "https://pbs.twimg.com/profile_images/496363117681455106/X2aE1GcC_reasonably_small.jpeg" },
+				{ title: "Guardians of the Galaxy", releaseDate: "15 September 2014", description: "description here", image: "https://pbs.twimg.com/profile_images/496363117681455106/X2aE1GcC_reasonably_small.jpeg" },
+				{ title: "Guardians of the Galaxy", releaseDate: "15 September 2014", description: "description here", image: "https://pbs.twimg.com/profile_images/496363117681455106/X2aE1GcC_reasonably_small.jpeg" },
+				{ title: "Guardians of the Galaxy", releaseDate: "15 September 2014", description: "description here", image: "https://pbs.twimg.com/profile_images/496363117681455106/X2aE1GcC_reasonably_small.jpeg" },
+				{ title: "Guardians of the Galaxy", releaseDate: "15 September 2014", description: "description here", image: "https://pbs.twimg.com/profile_images/496363117681455106/X2aE1GcC_reasonably_small.jpeg" }
 			]
 		}
 	};
+
+	//image content
+	MOVIEAPP.directives = {
+			image: {
+				src: function() {
+					return this.image;
+				}
+			}
+		}
 
 	//Controller Init
 	MOVIEAPP.controller = {
@@ -37,13 +51,13 @@ var MOVIEAPP = MOVIEAPP || {}; //Namespace
 		init:function () {
 			routie({
 				'/about': function() {
-					MOVIEAPP.page.about();
+					MOVIEAPP.sections.about();
 					console.log("route about");
 				},
 
 				'/movies': function() {
-					MOVIEAPP.page.movies();
-					console.log("route about");
+					MOVIEAPP.sections.movies();
+					console.log("route movies");
 				}
 
 			});
@@ -52,14 +66,14 @@ var MOVIEAPP = MOVIEAPP || {}; //Namespace
 		change: function () {
 			var route = window.location.hash.slice(2),
 			sections = qwery('section[data-route]'),
-			sections = qwery('[data-route=' + route + ']')[0];
+			section = qwery('[data-route=' + route + ']')[0];
 
 			//Show the section that is active(has the active class), hide the others
 			if (section) {
-			for(var i=0; i < sections.lenght; i++){
+				for(var i=0; i < sections.length; i++){
 				sections[i].classList.remove('active')
-			}
-			section.classList.add('active');
+				}
+				section.classList.add('active');
 			}
 
 			if(!route) {
@@ -71,14 +85,15 @@ var MOVIEAPP = MOVIEAPP || {}; //Namespace
 	//Page render
 	MOVIEAPP.sections = {
 		init: function(){
-
+			MOVIEAPP.sections.about();
+			MOVIEAPP.sections.movies();
 		},
 		about: function(){
 			Transparency.render(qwery('[data-route=about]')[0], MOVIEAPP.content.about);
 			MOVIEAPP.router.change();
 		},
 		movies: function(){
-			Transparency.render(qwery('[data-route=movies]')[0], MOVIEAPP.content.movies);
+			Transparency.render(qwery('[data-route=movies]')[0], MOVIEAPP.content.movies, MOVIEAPP.directives);
 			MOVIEAPP.router.change();
 		}
 	};
